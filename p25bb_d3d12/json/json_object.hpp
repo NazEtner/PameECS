ff854ddcede3e5967d707471d797198fb9f2dd12
NazEtner <113223856+NazEtner@users.noreply.github.com>
2025-12-03 03:20:05 +0900

    構造体とJSONを相互変換するための構造体の実装

---
+++ p25bb_d3d12/json/json_object.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "../template_types/string_literal.hpp"
+#include <nlohmann/json.hpp>
+
+namespace PameECS::JSON {
+	// 自動でJSONにシリアライズ、デシリアライズしたい
+	template <typename Derived>
+	struct JSONObject {
+		// CRTPを使ってJSONObjectを継承した構造体(多分クラスでもいい)をJSONへシリアライズする
+		nlohmann::json Serialize() {
+			nlohmann::json j;
+
+			static_cast<Derived*>(this)->ForEachMember(
+				[&]<TemplateTypes::StringLiteral Name>(auto& member) {
+					j[Name.data] = member;
+				}
+			);
+
+			return j;
+		}
+
+		// JSONからCRTPを使ってJSONObjectを継承した構造体(多分クラスでもいい)へデシリアライズする
+		// 構造体に存在するがJSONに存在しないプロパティは無視する
+		void Deserialize(const nlohmann::json& j) {
+			static_cast<Derived*>(this)->ForEachMember(
+				[&]<TemplateTypes::StringLiteral Name>(auto& member) {
+					if (j.contains(Name.data)) {
+						member = j.at(Name.data).get<std::remove_reference_t<decltype(member)>>();
+					}
+					// 見つからないなら元の値のまま(デフォルト値を使いたい場合を配慮する)
+				}
+			);
+		}
+	};
+}
