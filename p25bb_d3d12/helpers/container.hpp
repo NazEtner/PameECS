fdd01a55f8c7dff20e8133bfddd2f55718ba78a8
hirohumi shibata <hiro-shiba@outlook.com>
2025-12-16 11:47:17 +0900

    ResizePow2のループ条件を修正し不要なリサイズを防止
    whileループの条件を「<= minSize」から「< minSize」に変更し、containerサイズがminSizeと等しい場合に余分なリサイズが発生しないようにしました。

--- p25bb_d3d12/helpers/container.hpp
+++ p25bb_d3d12/helpers/container.hpp
@@ -5,7 +5,7 @@ namespace PameECS::Helper::Container {
 	void ResizePow2(T& container, size_t minSize, U init = U()) {
 		auto size = container.size();
 		size = size == 0 ? 1 : size;
-		while (size <= minSize) {
+		while (size < minSize) {
 			size *= 2;
 		}
 		container.resize(size, init);
