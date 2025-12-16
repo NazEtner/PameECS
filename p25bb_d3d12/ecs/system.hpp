1a201dc8f7434b75ca2e66d145f7cd48c8bdb218
NazEtner <113223856+NazEtner@users.noreply.github.com>
2025-12-16 22:12:23 +0900

    System::Base::Updateをnoexceptにした

--- p25bb_d3d12/ecs/system.hpp
+++ p25bb_d3d12/ecs/system.hpp
@@ -32,7 +32,7 @@ namespace PameECS::ECS::System {
 	public:
 		Base() = default;
 		virtual ~Base() = default;
-		virtual void Update(Context* context) = 0;
+		virtual void Update(Context* context) noexcept = 0;
 		virtual const Dependencies& GetDependencies() const = 0;
 	};
 }
