47a61f8b4c543e32b0eb58083249a4b4c20ed309
hirohumi shibata <hiro-shiba@outlook.com>
2025-12-24 19:42:09 +0900

    gamepadのクラスにちょっと書き足した

--- p25bb_d3d12/input/gamepad.hpp
+++ p25bb_d3d12/input/gamepad.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <memory>
 
 namespace PameECS::Input {
 	class Gamepad {
 	public:
+		Gamepad();
+		~Gamepad();
+		Gamepad(const Gamepad&) = delete;
+		Gamepad& operator=(const Gamepad&) = delete;
 	private:
+		struct Impl;
+		std::unique_ptr<Impl> m_impl;
 	};
 }
