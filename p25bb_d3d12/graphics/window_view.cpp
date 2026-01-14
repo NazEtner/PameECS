7c9585848c16353b25b4f7b8ff74fd07536dedb8
hirohumi shibata <hiro-shiba@outlook.com>
2026-01-14 16:08:40 +0900

    ウィンドウビューを作った

---
+++ p25bb_d3d12/graphics/window_view.cpp
@@ -0,0 +1,83 @@
+#include "window_view.hpp"
+
+using PameECS::Graphics::WindowView;
+using PameECS::Graphics::Window;
+using PameECS::Graphics::WindowSettingAdapter;
+using PameECS::Graphics::Renderer;
+
+extern "C" {
+	PECS_DLL_SHARED void GraphicsWindowViewSetVSyncEnabled(PameECS::Graphics::WindowView* windowView, bool isVSyncEnabled) {
+		windowView->SetVSyncEnabled(isVSyncEnabled);
+	}
+	PECS_DLL_SHARED bool GraphicsWindowViewIsVSyncEnabled(const PameECS::Graphics::WindowView* windowView) {
+		return windowView->IsVSyncEnabled();
+	}
+	PECS_DLL_SHARED void GraphicsWindowViewSetStyle(PameECS::Graphics::WindowView* windowView, const char* name, size_t nameSize) {
+		windowView->SetStyle(name, nameSize);
+	}
+	PECS_DLL_SHARED void GraphicsWindowViewSetWidth(PameECS::Graphics::WindowView* windowView, uint32_t width) {
+		windowView->SetWidth(width);
+	}
+	PECS_DLL_SHARED uint32_t GraphicsWindowViewGetWidth(const PameECS::Graphics::WindowView* windowView) {
+		return windowView->GetWidth();
+	}
+	PECS_DLL_SHARED void GraphicsWindowViewSetHeight(PameECS::Graphics::WindowView* windowView, uint32_t height) {
+		windowView->SetHeight(height);
+	}
+	PECS_DLL_SHARED uint32_t GraphicsWindowViewGetHeight(const PameECS::Graphics::WindowView* windowView) {
+		return windowView->GetHeight();
+	}
+}
+
+struct WindowView::Impl {
+	std::shared_ptr<Window> window;
+	std::shared_ptr<WindowSettingAdapter> adapter;
+	std::shared_ptr<Renderer> renderer;
+};
+
+WindowView::WindowView(
+	std::shared_ptr<Window>& window,
+	std::shared_ptr<WindowSettingAdapter> adapter,
+	std::shared_ptr<Renderer> renderer) {
+	m_impl = std::make_unique<Impl>();
+	m_impl->window = window;
+	m_impl->adapter = adapter;
+	m_impl->renderer = renderer;
+}
+
+void WindowView::SetVSyncEnabled(const bool isVSyncEnabled) {
+	m_impl->renderer->EnableVerticalSync(isVSyncEnabled);
+}
+
+bool WindowView::IsVSyncEnabled() const {
+	return m_impl->renderer->EnableVerticalSync(nullptr);
+}
+
+void WindowView::SetStyle(const char* name, size_t nameSize) {
+	std::string styleName(name, nameSize);
+	m_impl->adapter->SetStyle(styleName);
+}
+
+void WindowView::SetWidth(const uint32_t width) {
+	m_impl->adapter->SetWidth(width);
+}
+
+uint32_t WindowView::GetWidth() const {
+	auto properties = m_impl->window->GetProperties();
+	if (properties.width) {
+		return properties.width.value();
+	}
+	return 0;
+}
+
+void WindowView::SetHeight(const uint32_t height) {
+	m_impl->adapter->SetHeight(height);
+}
+
+uint32_t WindowView::GetHeight() const {
+	auto properties = m_impl->window->GetProperties();
+	if (properties.height) {
+		return properties.height.value();
+	}
+	return 0;
+}
---
+++ p25bb_d3d12/graphics/window_view.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "window.hpp"
+#include "window_setting_adapter.hpp"
+#include "renderer.hpp"
+#include "../macros/dll.hpp"
+#include <cstdint>
+#include <string>
+#include <memory>
+
+namespace PameECS::Graphics {
+	class WindowView;
+}
+
+extern "C" {
+	PECS_DLL_SHARED void GraphicsWindowViewSetVSyncEnabled(PameECS::Graphics::WindowView* windowView, bool isVSyncEnabled);
+	PECS_DLL_SHARED bool GraphicsWindowViewIsVSyncEnabled(const PameECS::Graphics::WindowView* windowView);
+	PECS_DLL_SHARED void GraphicsWindowViewSetStyle(PameECS::Graphics::WindowView* windowView, const char* name, size_t nameSize);
+	PECS_DLL_SHARED void GraphicsWindowViewSetWidth(PameECS::Graphics::WindowView* windowView, uint32_t width);
+	PECS_DLL_SHARED uint32_t GraphicsWindowViewGetWidth(const PameECS::Graphics::WindowView* windowView);
+	PECS_DLL_SHARED void GraphicsWindowViewSetHeight(PameECS::Graphics::WindowView* windowView, uint32_t height);
+	PECS_DLL_SHARED uint32_t GraphicsWindowViewGetHeight(const PameECS::Graphics::WindowView* windowView);
+}
+ 
+namespace PameECS::Graphics {
+	class WindowView {
+	public:
+		WindowView(std::shared_ptr<Window>& window, std::shared_ptr<WindowSettingAdapter> adapter, std::shared_ptr<Renderer> renderer);
+		~WindowView() = default;
+		void SetVSyncEnabled(const bool isVSyncEnabled);
+		bool IsVSyncEnabled() const;
+		void SetStyle(const std::string& name) {
+			SetStyle(name.c_str(), name.size());
+		}
+		void SetStyle(const char* name, size_t nameSize);
+		void SetWidth(const uint32_t width);
+		uint32_t GetWidth() const;
+		void SetHeight(const uint32_t height);
+		uint32_t GetHeight() const;
+	private:
+		struct Impl;
+		std::unique_ptr<Impl> m_impl;
+	};
+}
