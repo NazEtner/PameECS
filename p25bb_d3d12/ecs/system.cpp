67cd00dfb6e8f9b16f8cded1a77c33a4bb2bc8df
NazEtner <113223856+NazEtner@users.noreply.github.com>
2026-02-03 03:56:32 +0900

    Dependenciesの構成時にwrite又はreadがnullptrのときにUBを起こす可能性があったのを修正

--- p25bb_d3d12/ecs/system.cpp
+++ p25bb_d3d12/ecs/system.cpp
@@ -12,7 +12,9 @@ struct Dependencies::Impl {
 // exportするもの
 // これ以外はexportしない
 Dependencies::Dependencies(const size_t* write, const size_t writeSize, const size_t* read, const size_t readSize)
-	: Dependencies(std::vector<size_t>(write, write + writeSize), std::vector<size_t>(read, read + readSize)) {}
+	: Dependencies(
+		write ? std::vector<size_t>(write, write + writeSize) : std::vector<size_t>(),
+		read ? std::vector<size_t>(read, read + readSize) : std::vector<size_t>()) { }
 
 Dependencies::Dependencies(const std::vector<size_t>& write, const std::vector<size_t>& read) {
 	m_impl = std::make_unique<Impl>(write, read);
