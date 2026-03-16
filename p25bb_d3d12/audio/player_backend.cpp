fc5f8cac404ddbe7c27e6a16ac87975da38a6625
NazEtner <113223856+NazEtner@users.noreply.github.com>
2026-03-16 14:03:28 +0900

    もどした

--- p25bb_d3d12/audio/player_backend.cpp
+++ p25bb_d3d12/audio/player_backend.cpp
@@ -73,7 +73,6 @@ void PlayerBackend::Stop(size_t slotIndex) {
 	slot.sourceVoice->Stop();
 	slot.stopped = true;
 	slot.sourceVoice->FlushSourceBuffers();
-	slot.callback = nullptr;
 }
 
 void PlayerBackend::m_fillAndSubmit(size_t slotIndex) {
