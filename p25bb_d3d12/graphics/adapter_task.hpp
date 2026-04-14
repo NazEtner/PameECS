bc32786e4ff7f5897ddb17cd93b1baf8fe238b31
NazEtner <113223856+NazEtner@users.noreply.github.com>
2026-04-14 12:21:34 +0900

    レンダリングアダプターの設計ミスを修正。この修正により、これ以前のバージョンで書いたアダプタータスクとの互換性は廃止

--- p25bb_d3d12/graphics/adapter_task.hpp
+++ p25bb_d3d12/graphics/adapter_task.hpp
@@ -7,14 +7,14 @@ namespace PameECS::Graphics {
 		AdapterTask() = default;
 		virtual ~AdapterTask() = default;
 
-		using RenderCommandFunc = bool(*)(RendererTypes::RenderCommand, AdapterTask*);
+		using RenderCommandFunc = bool(*)(RendererTypes::RenderCommand, AdapterTask*, void*);
 
-		virtual RenderCommandFunc CreatePretreatmentCommand() = 0;
+		virtual RenderCommandFunc CreatePretreatmentCommand(void**) = 0;
 
 		// commandはnullptrではないはず
 		// 想定される呼び出し順は2回以上のEnqueueCommandの後に1回のCreateRenderCommandの繰り返し
 		// 最後にエンキューされたコマンドはCreateRenderCommandより前にEnqueueCommandがtrueを返していない場合以外に対応する呼び出しでは使用されるべきではない
-		virtual RenderCommandFunc CreateRenderCommand() = 0;
+		virtual RenderCommandFunc CreateRenderCommand(void**) = 0;
 
 		// 戻り値はそのコマンドを入力した直前までの状態でレンダリングコマンドを生成する必要があるか
 		// 実際はEnqueueした直後にレンダリングコマンドを生成するので、描画時に新しいコマンドを使わないように注意
